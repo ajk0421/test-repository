9811f57d39cc25cd98e1ec2b944acf511b21094a
ajk0421 <ajk.spirituallounge@gmail.com>
2018-10-01 10:50:11 +0900

    first_commit

---
+++ index.rb
@@ -0,0 +1,29 @@
+require "./menu"
+
+menu1 = Menu.new(name: "醤油ラーメン", price: 700)
+menu2 = Menu.new(name: "塩ラーメン", price: 700)
+menu3 = Menu.new(name: "味噌ラーメン", price: 800)
+menu4 = Menu.new(name: "毛ガニラーメン", price: 1200)
+
+menus = [menu1,menu2,menu3,menu4]
+
+index = 0
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+  index += 1
+end
+
+puts "---------------------"
+puts "何番にします？？"
+
+order = gets.chomp.to_i
+
+selected_menu = menus[order]
+
+puts"#{selected_menu.name}でいいかい？？"
+
+puts "何杯食べる？？なんと3杯食べたら１００円引き！"
+
+count = gets.chomp.to_i
+
+puts "お会計#{selected_menu.get_total_price(count)}です！"
